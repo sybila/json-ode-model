3e6c82aec4cd59c0871e9740f40414b695c919dc
Samuel Pastva <daemontus@gmail.com>
2016-09-04 21:47:07 +0200

    affinity tests

--- tests/lib.rs
+++ tests/lib.rs
@@ -72,3 +72,43 @@ fn unknown_evaluable() {
     let json = Json::from_reader(&mut file).unwrap();
     OdeModel::from_json(&json).unwrap();
 }
+
+#[test]
+fn multi_affine() {
+    assert!(OdeModel { name: "Affine".to_string(), parameters: vec!(), variables: vec!(
+        Variable { name: "var1".to_string(), range: Range { min: 1.0, max: 15.0 },
+                thresholds: vec!(2.0, 5.0), var_points: None,
+                equation: vec!(
+                    Summand { constant: 1.0, variable_indices: vec!(),
+                        parameter_indices: vec!(), evaluables: vec!(
+                            Evaluable::Step { variable_index: 0, theta: 0.9, a: 1.0, b: 2.0 },
+                            Evaluable::Ramp { variable_index: 0, low: 1.2, high: 3.3, a: 1.0, b: 2.0 },
+                            Evaluable::RampApproximation { variable_index: 0, approximation: vec!(
+                                Point { threshold: 2.0, value: 5.0 },
+                                Point { threshold: 3.0, value: 8.0 },
+                                Point { threshold: 4.0, value: 6.0 },
+                                Point { threshold: 6.0, value: 7.0 }
+                            )}
+                        )
+                    }
+                )
+            }
+    )}.is_multi_affine());
+}
+
+#[test]
+fn not_multi_affine() {
+    assert!(!OdeModel { name: "Not Affine".to_string(), parameters: vec!(), variables: vec!(
+        Variable { name: "var1".to_string(), range: Range { min: 1.0, max: 15.0 },
+                thresholds: vec!(2.0, 5.0), var_points: None,
+                equation: vec!(
+                    Summand { constant: 1.0, variable_indices: vec!(),
+                        parameter_indices: vec!(), evaluables: vec!(
+                            Evaluable::Hill { variable_index: 0, theta: 1.2, n: 5.0, a: 1.0, b: 2.0 },
+                            Evaluable::Sigmoid { variable_index: 1, theta: 2.2, k: 5.0, a: 1.0, b: 2.0 },
+                        )
+                    }
+                )
+            }
+    )}.is_multi_affine());
+}
